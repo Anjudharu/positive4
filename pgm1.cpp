5ea1c6a56bfaa2503bff21f397b84372410fa9a9
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-02-17 04:07:43 -0800

    Create pgm1.cpp

---
+++ pgm1.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int num;
+	cout<<"enter the number:";
+	cin>>num;
+	if(num>0)
+	cout<<"the number is positive";
+	else if(num<0)
+	cout<<"the number is negative";
+	else
+	cout<"thenumber is zero";
+	getch();
+}
