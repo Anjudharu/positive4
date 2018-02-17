6be28a30e5b70a215be1129a61d96a1423056b3a
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-02-17 04:24:03 -0800

    Create pgm3.cpp

---
+++ pgm3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namesapce std;
+void main()
+{
+	char a;
+	cout<<"enter the alphabet";
+	cin>>a;
+	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c='O' || c=='U')
+	cout<<"the alphabet is vowel";
+	else
+	cout<<"the alphabet is a consonant";
+	return 0;
+}
