67f3a320074e6cbad659b4d6eb08dbf721b3cefb
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-02-22 03:54:47 -0800

    Create 2pgm3.cpp

---
+++ 2pgm3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int num, i;
+bool isPrime = true;
+cout << "Enter a positive integer: ";
+cin >> num;
+for(i = 2; i <= num / 2; ++i)
+{
+if(n % i == 0)
+{
+isPrime = false;
+break;
+}
+}
+if (isPrime)
+cout << "This is a prime number";
+else
+cout << "This is not a prime number";
+return 0;
+}}
