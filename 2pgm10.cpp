7e4252c2bc056f7ad15ad553e11a389017a64978
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-02-23 04:30:57 -0800

    Create 2pgm10.cpp

---
+++ 2pgm10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+unsigned int num;
+unsigned long long factorial = 1;
+cout << "Enter a positive integer: ";
+cin >> num;
+for(int i=1;i<=num;++i)
+{
+factorial *=i;
+}
+cout<< "Factorial of "<< num << " = " << factorial;    
+return 0;
+}
