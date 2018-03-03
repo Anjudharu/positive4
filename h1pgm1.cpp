0e01f1580b4fc2f8336f297365a933e186854cf9
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-03-03 04:25:58 -0800

    Create h1pgm1.cpp

---
+++ h1pgm1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAXSIZE 10 
+void main()
+{
+    int array[MAXSIZE];
+    int i, j, n, temp;
+    printf("Enter the value of number \n");
+    scanf("%d", &n);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    printf("Input array is \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    for (i = 0; i< n; i++)
+    {
+        for (j = 0; j< (n - i - 1); j++)
+        {
+            if (array[j] >array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted array is...\n");
+    for (i =0; i< n; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+}
