a9684756d44566ba20fc4061ce288a47e5ca63f0
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-03-16 05:20:00 -0700

    Create 5pgm1.cpp

---
+++ 5pgm1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+{
+   char s[k];
+   int k = 0;
+cout<<"print k:"<<k;
+    gets(s);
+    while (s[k] != '\0') {
+      printf("%k", s[k]);
+      k++;
+   }
+    return 0;
+}
