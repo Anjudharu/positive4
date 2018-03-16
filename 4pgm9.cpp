cbee3cd0658a0dd90635e4b6f2740a1b4a056b0f
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-03-16 05:10:40 -0700

    Create 4pgm9.cpp

---
+++ 4pgm9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,large,x[10],temp,k,j;
+cout<<" Enter  10 numbers \n";
+for(i=0;i<10;i++)
+{
+cin >> a[i];
+}
+large=x[0];
+for (j=0;j<10;j++)
+{
+if (x[j]>large)
+{
+large=x[j];
+}
+}
+cout << " Largest is "<< large;
+return 0;
+}
