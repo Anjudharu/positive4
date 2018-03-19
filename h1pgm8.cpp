2bb845810a4e02334d2eae780334a0e656c32c19
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-03-19 05:29:54 -0700

    Create h1pgm8.cpp

---
+++ h1pgm8.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+struct eleCount
+{
+    int a;  
+    int b;  
+};
+void moreThanNdK(int arr[], int n, int k)
+{
+    if (k < 2)
+       return;
+    struct eleCount temp[k-1];
+    for (int i=0; i<k-1; i++)
+        temp[i].b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+       for (j=0; j<k-1; j++)
+        {
+            if (temp[j].a == arr[i])
+            {
+                 temp[j].b += 1;
+                 break;
+            }
+        }
+       if (j == k-1)
+        {
+            int l;
+            for (l=0; l<k-1; l++)
+            {
+                if (temp[l].b== 0)
+                {
+                    temp[l].a = arr[i];
+                    temp[l].b = 1;
+                    break;
+                }
+            }
+            if (l == k-1)
+                for (l=0; l<k; l++)
+                    temp[l].b-= 1;
+        }
+    }
+   for (int i=0; i<k-1; i++)
+    {
+      int ac = 0;  
+      for (int j=0; j<n; j++)
+            if (arr[j] == temp[i].a)
+                ab++;
+        if (ab > n/k)
+           cout << "Number:" << temp[i].a
+                << " Count:" << ab << endl;
+    }
+}
+int main()
+{
+    cout << "First Test\n";
+    int arr1[] = {4, 5, 6, 7, 8, 4, 4};
+    int size = sizeof(arr1)/sizeof(arr1[0]);
+    int k = 3;
+    moreThanNdK(arr1, size, k);
+    cout << "\nSecond Test\n";
+    int arr2[] = {4, 2, 2, 7};
+    size = sizeof(arr2)/sizeof(arr2[0]);
+    k = 3;
+    moreThanNdK(arr2, size, k);
+    cout << "\nThird Test\n";
+    int arr3[] = {2, 7, 2};
+    size = sizeof(arr3)/sizeof(arr3[0]);
+    k = 2;
+    moreThanNdK(arr3, size, k);
+    cout << "\nFourth Test\n";
+    int arr4[] = {2, 3, 3, 2};
+    size = sizeof(arr4)/sizeof(arr4[0]);
+    k = 3;
+    moreThanNdK(arr4, size, k);
+    return 0;
+}
