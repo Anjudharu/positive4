df5f13667898204e3499861bf7cb7cabb7695129
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-23 12:01:33 +0530

    Create 5pgm10.cpp

---
+++ 5pgm10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int tnum,num,flag=0;
+cout<<"enter the number:";
+cin>>n;
+tnum=num;
+if(n%2!=0)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+cout<<"the number is power of 2";
+}
+else
+{
+cout<<"the number is not a power of 2";
+}
+}
