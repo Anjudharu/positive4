ffaccde41c9bc5d38fa3a8b95cf50ebec4676fca
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-23 14:48:48 +0530

    Create 6pgm5.cpp

---
+++ 6pgm5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,n1,n2;
+cout<<"enter the numbers n1 and n2:";
+cin>>n1>>n2;
+n=n1*n2;
+if((n%2)==0)
+{
+cout<<"the product is even";
+}
+}
