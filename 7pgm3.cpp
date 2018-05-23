b68120a3a978cf7a130ee0e0dae8f1418e886e73
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-23 16:36:15 +0530

    Create 7pgm3.cpp

---
+++ 7pgm3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+str[50];
+int count=0,i;
+cout<<"enter string";
+gets(str);
+for (i = 0; str[i] != '\0';i++)
+{
+if(str[i]==' ')
+count++;
+}
+cout<<"the number of words";
+}
