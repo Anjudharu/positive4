4b42ebb03ad162a270c066aafe577ebec0fc4f13
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-23 14:30:56 +0530

    Create 6pgm4.cpp

---
+++ 6pgm4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+if((n%2)==0)
+{
+cout<<"the number is even";
+}
+else
+{
+n--;
+cout<<"the number is equal or less than n";
+}
+}
