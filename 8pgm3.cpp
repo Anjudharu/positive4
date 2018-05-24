460fb26e8fdc7bd9cc15b96eaff530bf8df8735e
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 11:49:07 +0530

    Create 8pgm3.cpp

---
+++ 8pgm3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;'
+int main()
+{
+int l,r,n;
+cout<<"enter the limits l and r:";
+cin>>l>>r;
+cout<<"enter the number:";
+cin>>n;
+if(n>=l&&n<=r)
+{
+cout<<"the number is in range";
+}
+else
+{
+cout<<"the number is not in range";
+}
+return 0;
+}
