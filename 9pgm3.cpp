031255f6c266004e8e358073ac2139111a7dd80a
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 16:01:16 +0530

    Create 9pgm3.cpp

---
+++ 9pgm3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n1,n2,div,mod;
+cout<<"enter the numbers n1 and n2:";
+cin>>n1>>n2>>endl;
+div=n1/n2;
+mod=n1%n2;
+cout<<div;
+cout<<mod;
+}
