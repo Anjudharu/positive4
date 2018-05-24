072fdd5f0103397194dfb574a9c5c7492a59a2a3
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 10:46:11 +0530

    Create 7pgm10.cpp

---
+++ 7pgm10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+if(n%2==0)
+{
+g=n+n;
+cout<<g;
+}
+else
+{
+cout<<"the number is not power of 2";
+}
+}
