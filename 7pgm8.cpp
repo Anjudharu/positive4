2d1c070b4030175026b486a9c34d0b13133f97c8
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 10:24:47 +0530

    Create 7pgm8.cpp

---
+++ 7pgm8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+if((n%7)==0)
+{
+cout<<"divisible by 7";
+}
+else
+{
+cout<<"not divisible  by 7";
+}
+}
