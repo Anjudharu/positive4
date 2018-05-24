49549ca662ec9dd917d3c62e4ccfaa4cb0d51b3e
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 15:41:32 +0530

    Create 9pgm1.cpp

---
+++ 9pgm1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int kc,oc,kc1,oc1,c1,c;
+cout<<"enter the number of ninjas in kabali clan and opponent clan:";
+cin>>kc>>oc;
+if(kc>oc)
+{
+c=kc-oc;
+}
+cout<<c;
+cout<<"enter kc1 and oc2";
+cin>>kc1>>oc2;
+if(kc1>oc2)
+{  c1=kc2-oc2;
+}
+cout<<c1;
+}
