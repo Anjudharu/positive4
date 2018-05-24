56128d709b441fe75ce3b3ba3ec9a043f358e710
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 14:02:59 +0530

    Create 8pgm8.cpp

---
+++ 8pgm8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter number:";
+cin>>n;
+if((n%13)==0)
+{
+cout<<n<<"multiple of 13";
+}
+else
+{
+cout<<n<<"not a multiple";
+}
+return 0;
+}
