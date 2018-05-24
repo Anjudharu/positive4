7332f142b8c0c2023a4e10a02e06fc15bdf208f7
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 09:44:58 +0530

    Create 7pgm4.cpp

---
+++ 7pgm4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,m,sum;
+cout<<"enter two numbers n and m:";
+cin>>n>>m;
+sum=n+m;
+cout<<sum;
+if((sum%2)==0)
+{
+cout<<"even";
+}
+else
+{
+cout<<"odd";
+}
+}
+
