ca0527bf65ace6d49c0475123a25a40c7948cd16
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-24 10:15:35 +0530

    Create 7pgm7.cpp

---
+++ 7pgm7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[25],n;
+cout<<"enter the number:";
+cin>>n;
+if((n<0)&&(n>=10)
+{
+cout<<"multiple is 10";
+}
+if((n<10)&&(n<=20))
+{
+cout<<"multiple is 20";
+}
+}
