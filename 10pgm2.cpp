21df4dae8da1dd1062ff5c5cbc99bbc6b0081c98
Anjudharu <36353074+Anjudharu@users.noreply.github.com>
2018-05-25 11:53:58 +0530

    Create 10pgm2.cpp

---
+++ 10pgm2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[i],n,sum=0;
+cout<<"enter number of elements:";
+cin>>n;
+for(i=0;i<n;i++)
+sum=sum+arr[i];
+cout<<sum;
+return 0;
+}
